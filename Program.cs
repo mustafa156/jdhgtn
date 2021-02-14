952b7d5de0748a46868ac2c29951220c6de47f72
mustafa aksoy <37409666+mustafa156@users.noreply.github.com>
2021-02-14 10:16:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace loops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			object[] ogrenciListesi = { };
+			for (int i = 0; i <= 100000; i++)
+			{
+				Console.WriteLine(i);
+
+			}
+			Console.WriteLine("finished");
+			
+			Console.ReadLine();
+		}
+	}
+}
